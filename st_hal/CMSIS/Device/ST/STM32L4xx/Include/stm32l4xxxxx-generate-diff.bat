fc5b3a0dc07107c63d3f2e6a5a6d31843b0606ca
Tilen Majerle <tilen.majerle@gmail.com>
2019-07-22 15:05:32 +0200

    All files with reorganized commit

---
+++ st_hal/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xxxxx-generate-diff.bat
@@ -0,0 +1,22 @@
+@color 0B
+@echo off
+
+REM Generate all CMSIS files 
+REM Active perl must be installed
+
+set scriptPATH=%CD%\..\..\..\..\..\..\..\__INTERNAL__tools\tools\_CmsisDeviceGenerator\
+
+echo %scriptPATH%
+
+if not exist "%scriptPATH%"  (
+	echo Input directory does not exist!
+	pause
+	exit
+)
+
+cd %scriptPATH%
+
+perl DeviceGeneration.pl  --target STM32L4xx -diff
+
+pause
+
